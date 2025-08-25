9c72f07e7423c0cb50ae5f96e60d3c7fb9f44a9a
Levchenko Artem <evgn.main@gmail.com>
2025-08-26 01:14:17 +0300

    Print and PrintAt functions

--- pkg/terminal/cursor.go
+++ pkg/terminal/cursor.go
@@ -15,9 +15,25 @@ func NewCursor() *Cursor {
 
 // MoveTo moves the cursor to the specified position
 func (t *Cursor) MoveTo(x, y int) {
-	t.x = x
-	t.y = y
+	t.x = x + 1
+	t.y = y + 1
 	fmt.Printf("\x1B[%d;%dH", t.y, t.x)
+}
+
+// Print prints a message at the current cursor position
+func (t *Cursor) Print(msg string) {
+	t.MoveTo(t.x, t.y)
+
+	fmt.Print(msg)
+
+	t.MoveTo(t.x+len(msg), t.y)
+}
+
+// PrintAt prints a message at the specified position
+func (t *Cursor) PrintAt(x, y int, msg string) {
+	t.MoveTo(x, y)
+
+	fmt.Print(msg)
 }
 
 // Reset resets the cursor to the top left corner
