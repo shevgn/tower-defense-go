6e5d3d3a2a935995c8ddcc9ff82db7751f70a286
Levchenko Artem <evgn.main@gmail.com>
2025-08-29 00:18:38 +0300

    test buffer

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,15 +2,44 @@
 package main
 
 import (
+	"os"
 	"time"
 
-	"tower-defense-go/pkg/terminal/examples"
+	"tower-defense-go/pkg/terminal"
+)
+
+const (
+	FPS           = 15
+	FrameDuration = time.Second / FPS
 )
 
 func main() {
-	examples.RunTypewriter(time.Millisecond * 1000)
+	trm := terminal.Default()
+	if err := trm.RawMode(); err != nil {
+		panic(err)
+	}
+	defer trm.Restore()
+	defer trm.Clear()
 
-	examples.RunRectangle(time.Millisecond * 2000)
+	trm.HideCursor()
+	defer trm.ShowCursor()
 
-	examples.RunLines(time.Millisecond * 2000)
+	r := terminal.NewRendererFrom(trm.Width(), trm.Height())
+	r.Buf.Fill(' ')
+	for x := 0; x < r.Buf.W; x++ {
+		r.Buf.Set(x, 0, terminal.BorderHorizontal)
+		r.Buf.Set(x, r.Buf.H-1, terminal.BorderHorizontal)
+	}
+	for y := 0; y < r.Buf.H; y++ {
+		r.Buf.Set(0, y, terminal.BorderVertical)
+		r.Buf.Set(r.Buf.W-1, y, terminal.BorderVertical)
+	}
+	r.Buf.Set(0, 0, terminal.BorderRoundedTopLeft)
+	r.Buf.Set(r.Buf.W-1, 0, terminal.BorderRoundedTopRight)
+	r.Buf.Set(0, r.Buf.H-1, terminal.BorderRoundedBottomLeft)
+	r.Buf.Set(r.Buf.W-1, r.Buf.H-1, terminal.BorderRoundedBottomRight)
+
+	_ = r.FlushFull(os.Stdout)
+
+	time.Sleep(time.Second * 2) // Sleep a bit to see the effect
 }
