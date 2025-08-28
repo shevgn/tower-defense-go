a408090440ac0e26035427f43f78eedf51fe2220
Levchenko Artem <evgn.main@gmail.com>
2025-08-29 00:45:59 +0300

    BorderAt as util

--- pkg/terminal/utils.go
+++ pkg/terminal/utils.go
@@ -48,3 +48,33 @@ func Typewriter(ctx context.Context, w *os.File, msg string, delay time.Duration
 	}()
 	return done
 }
+
+// BorderAt returns the border character at (x,y)
+func BorderAt(x, y, width, height int) (rune, bool) {
+	// TopLeft
+	if x == 0 && y == 0 {
+		return BorderRoundedTopLeft, true
+	}
+	// TopRight
+	if y == 0 && x == width-1 {
+		return BorderRoundedTopRight, true
+	}
+	// BottomLeft
+	if y == height-1 && x == 0 {
+		return BorderRoundedBottomLeft, true
+	}
+	// BottomRight
+	if y == height-1 && x == width-1 {
+		return BorderRoundedBottomRight, true
+	}
+	// Top
+	if y == 0 || y == height-1 {
+		return BorderHorizontal, true
+	}
+	// Left
+	if x == 0 || x == width-1 {
+		return BorderVertical, true
+	}
+
+	return ' ', false
+}
