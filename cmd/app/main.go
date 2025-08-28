f5ca8199117a199aa9111fb09e4f8eb7950e2fb4
Levchenko Artem <evgn.main@gmail.com>
2025-08-29 00:46:46 +0300

    rendering loop & handle Ctrl+(C/D)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/signal"
 	"time"
 
 	"tower-defense-go/pkg/terminal"
 )
 
 const (
-	FPS           = 15
+	FPS           = 60
 	FrameDuration = time.Second / FPS
 )
 
@@ -24,22 +27,86 @@ func main() {
 	trm.HideCursor()
 	defer trm.ShowCursor()
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	defer func() {
+		signal.Stop(sigCh)
+		close(sigCh)
+	}()
+
+	go func() {
+		select {
+		case <-sigCh:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	go func() {
+		buf := make([]byte, 1)
+		for {
+			n, err := os.Stdin.Read(buf)
+			if err != nil || n == 0 {
+				cancel()
+				return
+			}
+			b := buf[0]
+			if b == 0x03 { // Ctrl+C
+				cancel()
+				return
+			}
+			if b == 0x04 { // Ctrl+D (EOF)
+				cancel()
+				return
+			}
+			// handle other keys (arrows, enter, etc.)
+		}
+	}()
+
 	r := terminal.NewRendererFrom(trm.Width(), trm.Height())
-	r.Buf.Fill(' ')
-	for x := 0; x < r.Buf.W; x++ {
-		r.Buf.Set(x, 0, terminal.BorderHorizontal)
-		r.Buf.Set(x, r.Buf.H-1, terminal.BorderHorizontal)
-	}
-	for y := 0; y < r.Buf.H; y++ {
-		r.Buf.Set(0, y, terminal.BorderVertical)
-		r.Buf.Set(r.Buf.W-1, y, terminal.BorderVertical)
-	}
-	r.Buf.Set(0, 0, terminal.BorderRoundedTopLeft)
-	r.Buf.Set(r.Buf.W-1, 0, terminal.BorderRoundedTopRight)
-	r.Buf.Set(0, r.Buf.H-1, terminal.BorderRoundedBottomLeft)
-	r.Buf.Set(r.Buf.W-1, r.Buf.H-1, terminal.BorderRoundedBottomRight)
 
-	_ = r.FlushFull(os.Stdout)
+	drawBorder := func(buf *terminal.Buffer) {
+		for x := 0; x < buf.W; x++ {
+			buf.SetCh(x, 0, terminal.BorderHorizontal)
+			buf.SetCh(x, buf.H-1, terminal.BorderHorizontal)
+		}
+		for y := 0; y < buf.H; y++ {
+			buf.SetCh(0, y, terminal.BorderVertical)
+			buf.SetCh(buf.W-1, y, terminal.BorderVertical)
+		}
+		r.Buf.SetCh(0, 0, terminal.BorderRoundedTopLeft)
+		r.Buf.SetCh(r.Buf.W-1, 0, terminal.BorderRoundedTopRight)
+		r.Buf.SetCh(0, r.Buf.H-1, terminal.BorderRoundedBottomLeft)
+		r.Buf.SetCh(r.Buf.W-1, r.Buf.H-1, terminal.BorderRoundedBottomRight)
+	}
 
-	time.Sleep(time.Second * 2) // Sleep a bit to see the effect
+	ticker := time.NewTicker(FrameDuration)
+	defer ticker.Stop()
+
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			// final cleanup/animation
+			fmt.Fprintln(os.Stderr, "Exiting...")
+			return
+		case <-ticker.C:
+			r.Buf.Fill(' ')
+			drawBorder(r.Buf)
+
+			x := i % (r.Buf.W - 2)
+			r.Buf.SetCh(1+x, r.Buf.H/2, 'E')
+
+			if err := r.FlushFull(os.Stdout); err != nil {
+				fmt.Fprintln(os.Stderr, "Render error:", err)
+				cancel()
+				return
+			}
+
+			i++
+		}
+	}
 }
